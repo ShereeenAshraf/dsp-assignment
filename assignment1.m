77a97e9476c8447544d3d1a0a49c0a23d81b1d01
Shereen Ashraf <93353370+ShereeenAshraf@users.noreply.github.com>
2021-10-29 21:04:52 +0200

    Add files via upload

---
+++ assignment1.m
@@ -0,0 +1,29 @@
+%Generate Matrices A ,B ,C ,D ,
+A=[-7 5 -9 ;2 -1 2 ; 1 -1 2 ]
+B=[16 3 2 13 ;5 10 11 8; 9 6 7 12 ; 4 15 14 1 ]
+C=[4 2 -3 ;7 -7 9; 3 -5 6]
+D=[6 3 2 ; 2 12 -7 ;-1 6 2 ;-5 15 11]
+%Calculate the operations
+3*A-5*C
+C*A
+7*A + 2*B
+ C*D'
+ zeros(3)
+ zeros(4,2)
+ ones(2)
+ ones(4,3)
+ size(D)
+ zeros(size(D))
+ diag([1 2 3 4])
+ eye(3)
+[A,B] %ERROR becuse the rows not equil
+[A;B]%ERROR becuse the colums not equil
+ m=diag([5 5 5 5 5 5 5])
+  m(1:7,8)=[5;5;5;5;5;5;5]
+  size(m)
+  A(3,:)
+  A(:,2)
+A=[-7 5 -9  0;2 -1 2 0 ; 1 -1 2 0 ;0 0 0 0]
+size(A)
+ [A,B]
+ [A;B]
